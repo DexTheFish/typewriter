853631c019a1a093b2e63ed39ee7a1309511b680
Declan Davis <dexthefish@gmail.com>
2022-06-07 15:43:21 -0400

    commented out the original file contents. Rewrote the file as an exercise.

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,25 @@
 const sentence = "hello there from lighthouse labs";
+
 let delay = 0;
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, delay);
-  delay += 50; // <= 1s delay to make it noticeable. Can dial it down later.
+  delay += 50;
 }
 setTimeout(() => {
-  console.log('\n');
-}, delay + 50);
+  process.stdout.write("\n")
+}, delay);
+
+//---------- older version of the exercise -------------------
+
+// let delay = 0;
+// for (const char of sentence) {
+//   setTimeout(() => {
+//     process.stdout.write(char);
+//   }, delay);
+//   delay += 50; // <= 1s delay to make it noticeable. Can dial it down later.
+// }
+// setTimeout(() => {
+//   console.log('\n');
+// }, delay + 50);
